8fd73c21b94a9caea230c1418c3eb987e65bcd56
Rajan N <rajanofficial002@gmail.com>
2025-05-30 13:11:16 +0530

    Update To find the biggest square from the given land.py

--- To find the biggest square from the given land.py
+++ To find the biggest square from the given land.py
@@ -1,3 +1,12 @@
+"""
+A farmer wants to farm their land with the maximum area where good land is present.
+The "land" is represented as a matrix with 1s and Os, where 1s mean good land and Os
+mean bad land. The farmer only want to farm in a square of good land with the maximum
+area. Please help the farmer to find the maximum area of the land they can farm in
+good land.
+
+
+"""
 def function(i,j,dim):
     if len(land)>i+dim and len(land[0])>j+dim:
         for _ in range(dim):
@@ -11,7 +20,7 @@ def function(i,j,dim):
         return dim
 
 land=[[0,1,1,1,0,1],
-      [0,1,0,1,0,1],
+      [0,1,1,1,0,1],
       [0,1,1,1,1,1],
       [0,1,1,1,0,1]]
 
@@ -29,7 +38,14 @@ for i in range(len(land)):
                 j_=j
         else:
             score[-1].append(0)
-print(land)
-print(score)
-print(i_,j_)
-print(max_area)
+for i in range(len(land)):
+    for j in range(len(land[i])):
+        print(land[i][j],end=" ")
+    print()
+print()
+for i in range(len(score)):
+    for j in range(len(score[i])):
+        print(score[i][j],end=" ")
+    print()
+print()
+print(f"The biggest good square place is of length {max_area} square starts from {i_},{j_} to {i_+max_area-1},{j_+max_area-1}")
